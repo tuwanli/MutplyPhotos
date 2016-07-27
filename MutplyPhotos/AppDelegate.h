7c1e3d91e70b7e236b8601c546ea97a861601489
tuwanli <909111806@qq.com>
2016-07-27 08:46:57 +0800

    Initial Commit

---
+++ MutplyPhotos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MutplyPhotos
+//
+//  Created by 涂婉丽 on 16/7/27.
+//  Copyright © 2016年 涂婉丽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MutplyPhotos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MutplyPhotos
+//
+//  Created by 涂婉丽 on 16/7/27.
+//  Copyright © 2016年 涂婉丽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
